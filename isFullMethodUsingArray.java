9b3373444d050fc7181154ad3c1f4b3b8b422f44
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-23 09:39:29 +0530

    Create isFullMethodUsingArray.java

---
+++ isFullMethodUsingArray.java
@@ -0,0 +1,48 @@
+.................
+Main.java
+.................
+class Main{
+    public static void main(String[] args){
+    Stack newStack = new Stack(4);
+    boolean result = newStack.isFull();
+    System.out.println(result);
+
+    }
+}
+..................................
+Stack.java
+..................................
+public class Stack {
+    int[] arr;
+    int topOfStack;
+
+    public Stack(int size){
+        this.arr = new int[size];
+        this.topOfStack =-1;
+        System.out.println("The Stack is created with size of : " + size);
+        
+    }
+
+    // isEmpty
+    public boolean isEmpty(){
+        if(topOfStack==-1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //isFull method
+    public boolean isFull(){
+        if(topOfStack==arr.length-1){
+            System.out.println("The Stack is full");
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+}
+
+...........
+//////finish///////////
