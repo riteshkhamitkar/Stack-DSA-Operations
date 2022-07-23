41bc5574547e305604e7826b8036cb3b56e6b474
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-23 09:56:06 +0530

    Create popUsingArrayinStack.java

---
+++ popUsingArrayinStack.java
@@ -0,0 +1,80 @@
+.................
+Main.java
+.................
+class Main{
+    public static void main(String[] args){
+    Stack newStack = new Stack(4);
+    newStack.push(1);
+    newStack.push(2);
+    newStack.push(3);
+    newStack.push(4);
+    int result = newStack.pop();
+    System.out.println(result);
+
+    int result1 = newStack.pop();
+    System.out.println(result1);
+    }
+}
+..................................
+Stack.java
+..................................
+public class Stack {
+    int[] arr;
+    int topOfStack;
+
+    public Stack(int size){
+        this.arr = new int[size];
+        this.topOfStack =-1;
+        System.out.println("The Stack is created with size of : " + size);
+        
+    }
+
+    // isEmpty
+    public boolean isEmpty(){
+        if(topOfStack==-1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //isFull method
+    public boolean isFull(){
+        if(topOfStack==arr.length-1){
+            System.out.println("The Stack is full");
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //Push
+
+    public void push(int value){
+        if(isFull()){
+            System.out.println("The Stack is Full!");
+        }else{
+            arr[topOfStack+1]=value;
+            topOfStack++;
+            System.out.println("The value is successfully inserted!");
+        }
+    }
+
+    //Pop 
+
+    public int pop(){
+        if(isEmpty()){
+            System.out.println("The Stack is Empty!");
+            return -1;
+
+        }else{
+            int topStack = arr[topOfStack];
+            topOfStack--;
+            return topStack;
+        }
+    }
+    
+}
+
+...........
+//////finish///////////
